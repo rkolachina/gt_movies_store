7d107f54e512e07ad91af4a6cd100427b42679c8
rkolachina <roshan@kolachina.com>
2025-02-03 20:32:43 -0500

    created register

--- gt_movies_store/authentication/views.py
+++ gt_movies_store/authentication/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
+def register (request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            ##return redirect home
+        else:
+            form = UserCreationForm()
+        ##redirect user
