b03977ffe93251b678e6dd51bd59cffdddb3a564
Krish Prasad <krishkp00@gmail.com>
2025-02-05 23:39:22 -0500

    commit

--- gt_movies_store/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('signup', views.register, name='accounts.signup'),
-    path('login/', views.login, name='accounts.login'),
-    #path('logout/', views.logout, name='accounts.logout'),
-    #path('orders/', views.orders, name='accounts.orders'),
-]
